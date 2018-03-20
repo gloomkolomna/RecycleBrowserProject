b320722ba16862f053d5e4bdec7a948759aebb6a
cinik89 <thrupers@gmail.com>
2018-03-20 22:49:47 +0300

    first

---
+++ RecycleProject/Controllers/RecycleMapController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecycleProject.Model;
+
+namespace RecycleProject.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/RecycleMap")]
+    public class RecycleMapController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetPoint()
+        {
+            RecyclePoint point = new RecyclePoint()
+            {
+                Name = "First",
+                Description = "First Point in first Core application.",
+                Location = new Location
+                {
+                    Latitude = 0d,
+                    Longitude = 1d
+                }
+            };
+
+            return Json(point);
+        }
+    }
+}
---
+++ RecycleProject/Model/RecyclePoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecycleProject.Model
+{
+    public class RecyclePoint
+    {
+        public RecyclePoint()
+        {
+
+        }
+
+        public string Name { get; set; }
+        public Location Location { get; set; }
+        public string Description { get; set; }
+    }
+}
