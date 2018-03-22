be1b713f53a37d71a2db88715594d9915e3527d8
cinik89 <thrupers@gmail.com>
2018-03-22 22:31:36 +0300

    Add dependencies

--- RecycleProject/Startup.cs
+++ RecycleProject/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RecycleProject.Interfaces;
 
 namespace RecycleProject
 {
@@ -24,6 +26,9 @@ namespace RecycleProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<RecycleContext>(options => options.UseSqlite("Data Source=recycle.db"));
+            services.AddTransient<IRepository, Repository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
